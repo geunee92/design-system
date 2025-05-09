168d5b9c9fd14505d287d0eb4218c36a10992099
Hyungeun Jo <geunee92@ship-da.com>
2025-05-09 15:32:03 +0900

    feat: (react-components-button): 버튼 컴포넌트 추가

---
+++ services/storybook/stories/ReactComponents/Button/Button.stories.tsx
@@ -0,0 +1,35 @@
+import "@design/react-components-button/style.css";
+import { Button as _Button } from "@design/react-components-button";
+import { vars } from "@design/themes";
+
+export default {
+  title: "React Components/Button",
+  component: _Button,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    size: {
+      options: ["xs", "sm", "md", "lg"],
+      control: "select",
+    },
+    color: {
+      options: Object.keys(vars.colors.$scale),
+      control: "select",
+    },
+    variant: {
+      options: ["solid", "outline", "ghost"],
+      control: "select",
+    },
+  },
+};
+
+export const ButtonStory = {
+  args: {
+    size: "lg",
+    children: "Button",
+    variant: "outline",
+    isDisabled: false,
+  },
+};
