ca8cf86c5ea1c8368f774360bb5d7da12c01024c
Hyungeun Jo <geunee92@ship-da.com>
2025-05-12 10:58:46 +0900

    feat(react-components-input): 인풋 애드온 기능 추가

--- services/storybook/stories/ReactComponents/Input/Input.stories.tsx
+++ services/storybook/stories/ReactComponents/Input/Input.stories.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "@design/react-components-input/style.css";
-import { Input } from "@design/react-components-input";
+import {
+  Input,
+  InputGroup,
+  InputLeftAddon,
+} from "@design/react-components-input";
 
 export default {
   title: "React Components/Input",
@@ -12,4 +16,13 @@ export default {
 
 export const InputStory = {
   render: () => <Input placeholder="dd" />,
+};
+
+export const InputGroupStory = {
+  render: () => (
+    <InputGroup size="lg" color="red">
+      <InputLeftAddon>$</InputLeftAddon>
+      <Input placeholder="dd" />
+    </InputGroup>
+  ),
 };
