f77e3856b3ed1dfa59f347213e9a211539e0a7a2
Hyungeun Jo <geunee92@ship-da.com>
2025-05-14 14:10:20 +0900

    feat(service-page-builder): 모바일 우선 레이아웃 추가

--- services/page-builder/src/pages/playground.tsx
+++ services/page-builder/src/pages/playground.tsx
@@ -1,13 +1,14 @@
-import { Box } from "@design/react-components-layout";
 import { vars } from "@design/themes";
-import { CDN_BASE_URL } from "../constatns";
+import { CDN_BASE_URL } from "../constants";
+import { MobileFirstLayout } from "../components/view/MobileFirstLayout";
 
 const PlaygroundPage = () => {
   return (
-    <Box padding={3}>
-      <h1>{vars.colors.$scale.gray[900]}</h1>
+    <MobileFirstLayout>
+      <h1>{vars.colors.$static.light.blue[500]}</h1>
+
       <img src={`${CDN_BASE_URL}/test/hero_mobile.webp`} alt="" />
-    </Box>
+    </MobileFirstLayout>
   );
 };
 
