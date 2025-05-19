13618522e74060d5239d354b82403f0b35cded50
Hyungeun Jo <geunee92@ship-da.com>
2025-05-19 15:06:13 +0900

    refactor(services-page-builder): 에디터 페이지 추상화 레벨 수정

--- services/page-builder/src/pages/editor/new/json.tsx
+++ services/page-builder/src/pages/editor/new/json.tsx
@@ -11,7 +11,7 @@ import { previewStorage } from "@/src/utils/storage";
 import { useToast } from "@design/react-components-toast";
 import { useViewSchemaValidation } from "@/src/hooks/useViewSchemaValidation";
 import { DesktopFirstSideNav } from "@/src/components/Common/Layouts/DesktopFirstLayout/SideNav";
-import { JsonPresetList } from "@/src/components/Features/EditorNewPage/JsonPresetList";
+import { JsonPresetList } from "@/src/components/Features/EditorNewJsonPage/JsonPresetList";
 import { putViewDetail } from "@/src/apis/worker/putViewDetail";
 
 const EditorNewJsonPage: React.FC = () => {
