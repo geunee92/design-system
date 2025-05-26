3b74304933053f1adaa5d7209c776da4c2957ee2
Hyungeun Jo <geunee92@ship-da.com>
2025-05-26 21:24:25 +0900

    feat(services-youtube): 검색 결과 목록API 추가

--- services/youtube/src/features/main/api/getVideosPopularList.ts
+++ services/youtube/src/features/main/api/getVideosPopularList.ts
@@ -1,6 +1,8 @@
 import { API_BASE_URL } from "@/src/shared/api/constants";
-import { VideoThumbnail } from "@/src/shared/api/youtube/types/item";
-import { ListPageApiInfo } from "@/src/shared/api/youtube/types/list";
+import {
+  ListPageApiInfo,
+  VideoListItem,
+} from "@/src/shared/api/youtube/types/list";
 import { youtube_v3 } from "googleapis";
 import queryString from "query-string";
 
@@ -10,17 +12,9 @@ export type GetVideosPopularListRequestParams = Pick<
 >;
 
 export type PopularListItem = {
-  videoId: string;
-  title: string;
-  description: string;
-  channelId: string;
-  channelTitle: string;
-  thumbnail: VideoThumbnail;
-  publishedAt: string;
-  publishedAtDisplayText: string; // 1개월 전
   viewCount: number;
   viewCountDisplayText: string; // 23만
-};
+} & VideoListItem;
 
 export type GetVideosPopularListResponse = {
   lists: PopularListItem[];
