30c655d44978febdc18d5e67021a2707d1f29750
Hyungeun Jo <geunee92@ship-da.com>
2025-05-27 22:57:22 +0900

    feat(services-youtube): 검색 결과 목록 아이템 추가

--- services/youtube/src/app/(youtube)/search/page.tsx
+++ services/youtube/src/app/(youtube)/search/page.tsx
@@ -1,9 +1,15 @@
 import { SearchFilter } from "@/src/features/search/components/SearchFilter";
+import { SearchResultList } from "@/src/features/search/components/SearchResultList";
+import { Suspense } from "react";
 
 export default function Search() {
   return (
     <main>
       <SearchFilter />
+
+      <Suspense fallback={<></>}>
+        <SearchResultList />
+      </Suspense>
     </main>
   );
 }
